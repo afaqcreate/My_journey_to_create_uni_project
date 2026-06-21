66cff079594364bab1cff3056e3aaf9c653f7ceb
Afaque R <120298348+afaqcreate@users.noreply.github.com>
2026-06-21 20:14:26 +0500

    loops

---
+++ New folder/loops.js
@@ -0,0 +1,11 @@
+var i = 0
+while(i <=3 ){
+    console.log("You'r value is", i);
+    i++;
+}
+
+console.log("--------------------------------------")
+
+for(var a = 1; a <= 5; a++){
+    console.log("This is value of A", a);
+}
